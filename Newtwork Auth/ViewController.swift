54e65e44923aed203e78f216885016dd09d650c8
Warren Hansen <whansen1@mac.com>
2018-01-10 17:35:27 -0800

    task: getting data and parsing date, close

--- Newtwork Auth/ViewController.swift
+++ Newtwork Auth/ViewController.swift
@@ -7,19 +7,63 @@
 //
 
 import UIKit
-
+//import Foundation
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        let user = "someUser"
+        let password = "somePassword"
+        callNetwork(ticker: "AAPL", user: user, password: password)
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    //MARK: - TODO - Refactor to network file
+    func callNetwork(ticker:String, user:String, password:String) {
+        
+        let loginData = String(format: "%@:%@", user, password).data(using: String.Encoding.utf8)!
+        let base64LoginData = loginData.base64EncodedString()
+        let urlString = "https://api.intrinio.com/prices?ticker=\(ticker)"
+        // create the request
+        let url = URL(string: urlString)!
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        request.setValue("Basic \(base64LoginData)", forHTTPHeaderField: "Authorization")
+        
+        //making the request
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data, error == nil else {
+                print("\(error.debugDescription)")
+                return
+            }
+            
+            let jsonObj = try! JSONSerialization.jsonObject(with: data, options: .allowFragments) as? NSDictionary
+            print(jsonObj!.value(forKey: "data")!)
+            if let priceArray = jsonObj!.value(forKey: "data") as? NSArray {
+                //looping through all the elements
+                for each in priceArray {
+                    
+                    //converting the element to a dictionary
+                    if let priceDict = each as? NSDictionary {
+                        //getting the name from the dictionary
+                        if let date = priceDict.value(forKey: "date"), let close = priceDict.value(forKey: "close") {
+                            print(date, close)
+                        }
+                        
+                    }
+                }
+            }
+            
+            
+            if let httpStatus = response as? HTTPURLResponse {
+                // check status code returned by the http server
+                print("status code = \(httpStatus.statusCode)")
+                // process result
+            }
+        }
+        task.resume()
     }
-
 
 }
 
